8371f1d9c4c003e515b3271e57d55d35853284c5
Jallappino <stefan.murakami@gmail.com>
2018-03-08 21:30:53 -0500

    third finished

--- src/main/resources/static/src/js/app.js
+++ src/main/resources/static/src/js/app.js
@@ -14,7 +14,8 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		const res = JSON.parse(xhr.responseText); 
-		document.querySelector('main').innerHTML = "<p>" + res.name + "</p>";
+		document.querySelector('main').innerHTML = "<p>" + res.name + "</p>"
+		+ "<p>" + res.height + "</p>" + "<p>" + res.homeworld + "</p>"; 
 	}
 };
 xhr.open("GET", "https://swapi.co/api/people/1/", true);
@@ -22,6 +23,14 @@ xhr.send();
 
 // Create a new CSS sheet with new style rules and
 // update elements on the page 10 seconds after the page loads
+
+//$(elem).css('z-index', '0'); 
+//setTimeout(function() { $(elem).css('z-index','2'); }, 2000)
+
+const mainStyle = document.querySelector('main'); 
+const changeStyle = () => mainStyle.style.backgroundColor = 'red'; 
+
+setTimeout(changeStyle, 10000); 
 
 // Find another public API to get data from and use it to add content to your page
 
